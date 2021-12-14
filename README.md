c14970523cbc412f77e9be5f12649cc680ae7882
Thomas <pianist.chopin.1@gmail.com>
2021-12-14 16:53:06 +0800

    Add sql, postman json, Readme

---
+++ README.md
@@ -0,0 +1,51 @@
+# My Express Typescript Starter Boilerplate
+
+-NodeJs
+-Typescript
+-Express
+-TypeORM
+-Mysql
+
+Mysql - test.sql
+postman json- My-Express-Typescript-Starter.postman_collection.json
+
+API
+
+/signup
+
+/login
+
+/logout
+
+/users (get)
+
+/users (post)
+
+/users/:id (put)
+
+/users/:id (delete)
+
+## Setup
+```
+npm install
+```
+
+## Development
+```
+npm run dev
+```
+
+## Build
+```
+npm run build
+```
+
+## Production
+```
+npm run start
+```
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)]
+
+http://tenx.engineer/
+Thomas Peters
---
+++ test.sql
@@ -0,0 +1,177 @@
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	10.4.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `batch` int(11) DEFAULT NULL,
+  `migration_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'20210713110926_initial.ts',1,'2021-12-12 19:32:57');
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations_lock`
+--
+
+DROP TABLE IF EXISTS `migrations_lock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations_lock` (
+  `index` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `is_locked` int(11) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations_lock`
+--
+
+LOCK TABLES `migrations_lock` WRITE;
+/*!40000 ALTER TABLE `migrations_lock` DISABLE KEYS */;
+INSERT INTO `migrations_lock` VALUES (1,0);
+/*!40000 ALTER TABLE `migrations_lock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'gfdsg','sdfg'),(2,'sfdg','fdsg'),(3,'fdg','sfdg'),(4,'pianist.chopin.1@gmail.com','$2b$10$QElWnTiEDeFjgXPaCWQ7r.0OC9G5iTiWw8LGD2l4.Cz6kzDRbzOk.'),(5,'bbbb.c@gmail.com','$2b$10$viQzrIgLO4DI1yhceYNTDer9W1KLyJw2NkyW6dr9O57aVrv/iVyn6'),(6,'aaa.aa@gmail.com','$2b$10$eCTPclpKpLpslgOMzsZeretfdgzFbwA4dHprkUUKhCbZWGa/AYM7y'),(7,'bbbb.bb@gmail.com','$2b$10$5nLc/O5/8ReQWG0CQvk6dOglNo.9qbK.mJ2VUa2C1C3wqrcgapuBq');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_entity`
+--
+
+DROP TABLE IF EXISTS `user_entity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_entity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_entity`
+--
+
+LOCK TABLES `user_entity` WRITE;
+/*!40000 ALTER TABLE `user_entity` DISABLE KEYS */;
+INSERT INTO `user_entity` VALUES (1,'hgfdh','fghfg'),(2,'dgfh','fghg'),(3,'','');
+/*!40000 ALTER TABLE `user_entity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userentity`
+--
+
+DROP TABLE IF EXISTS `userentity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userentity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userentity`
+--
+
+LOCK TABLES `userentity` WRITE;
+/*!40000 ALTER TABLE `userentity` DISABLE KEYS */;
+INSERT INTO `userentity` VALUES (1,'dfsgfg','sdfgdfsgfg'),(2,'sdfgdfg','dfgsgrer'),(3,'fdsg','sdfg');
+/*!40000 ALTER TABLE `userentity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'sdafsdf','asdfsadf');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-14 16:38:37
